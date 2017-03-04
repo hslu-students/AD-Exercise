17299ee22dd8965a4228706c4525b721e6bfda35
Timo Furrer <tuxtimo@gmail.com>
2017-03-04 14:28:24 +0100

    Implement simple stack using arrays

---
+++ src/main/java/ch/hslu/AD/SW2/ArrayStack/ArrayStack.java
@@ -0,0 +1,57 @@
+package ch.hslu.AD.SW2.ArrayStack;
+
+import java.util.NoSuchElementException;
+
+import ch.hslu.AD.SW2.ArrayStack.StackFullException;
+
+public class ArrayStack<ElementType> {
+	private int size = 0;
+	private int index = 0;
+	private ElementType[] stack;
+	
+	public ArrayStack(int size) {
+		this.size = size;
+		
+		// See http://stackoverflow.com/a/530289/1336014
+		@SuppressWarnings("unchecked")
+		final ElementType[] stack = (ElementType[]) new Object[size];
+		this.stack = stack;
+	}
+	
+	public boolean isEmpty() {
+		return index == 0;
+	}
+	
+	public boolean isFull() {
+		return index == size;
+	}
+	
+	public int size() {
+		return this.size;
+	}
+
+	public boolean push(ElementType element) throws StackFullException {
+		if(isFull()) {
+			throw new StackFullException(size);
+		}
+		
+		stack[index++] = element;
+		return true;
+	}
+	
+	public ElementType pop() {
+		if(isEmpty()) {
+			throw new NoSuchElementException("Stack is empty");
+		}
+
+		return stack[--index]; // it's not really necessary to actually remove the element from the stack.
+	}
+	
+	public ElementType peek() {
+		if(isEmpty()) {
+			return null;
+		}
+		
+		return stack[index - 1];
+	}
+}
---
+++ src/test/java/ch/hslu/AD/SW2/ArrayStack/ArrayStackTest.java
@@ -0,0 +1,74 @@
+package ch.hslu.AD.SW2.ArrayStack;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import ch.hslu.AD.SW2.ArrayStack.ArrayStack;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Unit test for simple App.
+ */
+public class ArrayStackTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testCreatingStack() {
+    	ArrayStack<String> stack = new ArrayStack<>(5);
+    	assertTrue(stack.isEmpty());
+    }
+    
+    @Test
+    public void testAddingElements() throws Exception {
+    	ArrayStack<String> stack = new ArrayStack<>(5);
+    	stack.push("Hodor");
+    	assertFalse(stack.isEmpty());
+    }
+    
+    @Test
+    public void testFillingStack() throws Exception {
+    	ArrayStack<String> stack = new ArrayStack<>(1);
+    	stack.push("Hodor");
+    	assertTrue(stack.isFull());
+    }
+    
+    @Test
+    public void testPushAndPop() throws Exception {
+    	ArrayStack<String> stack = new ArrayStack<>(3);
+    	stack.push("toll");
+    	stack.push("sind");
+    	stack.push("Datenstrukturen");
+    	
+    	assertEquals("Datenstrukturen", stack.pop());
+    	assertEquals("sind", stack.pop());
+    	assertEquals("toll", stack.pop());
+    }
+    
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+    
+    @Test
+    public void testStackFull() throws StackFullException {
+    	ArrayStack<String> stack = new ArrayStack<>(1);
+    	stack.push("Hodor");
+    	
+    	exception.expect(StackFullException.class);
+    	stack.push("White Walker");
+    }
+    
+    @Test
+    public void testPeek() throws StackFullException {
+    	ArrayStack<String> stack = new ArrayStack<>(1);
+    	stack.push("Hodor");
+    	assertEquals("Hodor", stack.peek());
+    	assertFalse(stack.isEmpty());
+    	assertEquals("Hodor", stack.pop());
+    	assertTrue(stack.isEmpty());
+    }
+}
